d178af9d668a33fe236415572cecbbf5aad628ab
pavlovskayajulia1937 <pavlovskayajulia1937@gmail.com>
2025-08-01 22:20:44 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,11 @@
+import pytest
+from selenium import webdriver
 
+
+@pytest.fixture(scope="function")
+def driver():
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield driver
+    driver.quit()
