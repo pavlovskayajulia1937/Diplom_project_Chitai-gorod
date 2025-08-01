0333ffd7ac9633d51c888c7b8ce6a9b59a233fbe
pavlovskayajulia1937 <pavlovskayajulia1937@gmail.com>
2025-08-01 22:05:02 +0300

    Update environment.py

--- config/environment.py
+++ config/environment.py
@@ -1,4 +1,4 @@
 class Environment:
     BASE_URL = "https://www.chitai-gorod.ru/"
-    API_URL = f"{BASE_URL}api/v2/search/facet-search"
+    API_URL = "https://web-gate.chitai-gorod.ru/api/v2/search/facet-search"
     DEFAULT_CITY_ID = 213
